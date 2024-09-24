664025d78e1e638c65844b8371ae9edae8eec067
eamrandoc <eamrandoc@gmail.comm>
2024-09-25 01:23:33 +0600

    added validation in html and js

--- js/index.js
+++ js/index.js
@@ -6,15 +6,30 @@
 // const quotaBalance = document.getElementById('quota-balance').innerText;
 // const quotaFund = document.getElementById('quota-fund').value;
 // console.table({mainBalance, noakhaliBalance,noakhaliFund,feniBalance,feniFund,quotaBalance,quotaFund});
+
+
 // 1st row 
 document.getElementById('noakhali-donate').addEventListener('click', function () {
     const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
     const noakhaliBalance = parseFloat(document.getElementById('noakhali-balance').innerText);
-    const noakhaliFund = parseFloat(document.getElementById('noakhali-fund').value);
+    const noakhaliFund = parseFloat(document.getElementById('noakhali-fund').value);   
+
+       if (isNaN(noakhaliFund) || noakhaliFund <= 0 || mainBalance < noakhaliFund || !isFinite(document.getElementById('noakhali-fund').value)) {
+        alert('Invalid Input')
+        return;
+    };
+
     const addBalance = noakhaliBalance + noakhaliFund ;
     document.getElementById('noakhali-balance').innerText = addBalance.toFixed(2) ;
     const remainBalance = mainBalance - noakhaliFund;
-    document.getElementById('main-balance').innerText = remainBalance.toFixed(2);
+    document.getElementById('main-balance').innerText = remainBalance.toFixed(2); 
+
+    my_modal_5.showModal();
+
+    document.getElementById('modal-btn').addEventListener('click', function () {
+        
+        document.getElementById('noakhali-fund').value = '';
+    });
     
     
     const historyContainer = document.createElement('div');
@@ -24,10 +39,7 @@ document.getElementById('noakhali-donate').addEventListener('click', function ()
         <p class="font-light text-primaryText text-opacity-70"> Date: ${new Date()}</p>    
     `
     const historyList= document.getElementById('history-list');
-    historyList.insertBefore(historyContainer , historyList.firstChild);
-
-
-   
+    historyList.insertBefore(historyContainer , historyList.firstChild);   
 
 })
 
@@ -36,11 +48,24 @@ document.getElementById('feni-donate').addEventListener('click', function () {
     const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
     const feniBalance = parseFloat(document.getElementById('feni-balance').innerText);
     const feniFund = parseFloat(document.getElementById('feni-fund').value);
+
+    if (isNaN(feniFund) || feniFund <= 0 || mainBalance < feniFund || !isFinite(document.getElementById('feni-fund').value)) {
+        alert('Invalid Input')
+        return;
+    }
+
     const addBalance = feniBalance + feniFund ;
     document.getElementById('feni-balance').innerText = addBalance.toFixed(2) ;
     const remainBalance = mainBalance - feniFund;
     document.getElementById('main-balance').innerText = remainBalance.toFixed(2);
 
+    my_modal_5.showModal();
+    document.getElementById('modal-btn').addEventListener('click', function () {
+        
+        document.getElementById('feni-fund').value = '';
+
+    })
+    
     const historyContainer = document.createElement('div');
     historyContainer.className= 'border border-solid border-borderbg rounded-2xl p-8';
     historyContainer.innerHTML = `
@@ -56,10 +81,23 @@ document.getElementById('quota-donate').addEventListener('click', function () {
     const mainBalance = parseFloat(document.getElementById('main-balance').innerText);
     const quotaBalance = parseFloat(document.getElementById('quota-balance').innerText);
     const quotaFund = parseFloat(document.getElementById('quota-fund').value);
+
+    if (isNaN(quotaFund) || quotaFund <= 0 || mainBalance < quotaFund || !isFinite(document.getElementById('quota-fund').value)) {
+        alert('Invalid Input')
+        return;
+    }
+
+
     const addBalance = quotaBalance + quotaFund ;
     document.getElementById('quota-balance').innerText = addBalance.toFixed(2) ;
     const remainBalance = mainBalance - quotaFund;
     document.getElementById('main-balance').innerText = remainBalance.toFixed(2);
+
+    my_modal_5.showModal();
+    document.getElementById('modal-btn').addEventListener('click', function () {
+        document.getElementById('quota-fund').value = '';      
+    })
+    
 
     const historyContainer = document.createElement('div');
     historyContainer.className= 'border border-solid border-borderbg rounded-2xl p-8';
